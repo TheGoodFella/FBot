934b42f6a4c6a2498decd039bee023faf91172b5
TheGoodFella <galas.daniele@gmail.com>
2016-09-11 16:46:24 +0200

    add a language manager

--- FBot/FBotInterface/MainForm.Designer.cs
+++ FBot/FBotInterface/MainForm.Designer.cs
@@ -32,6 +32,7 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.accountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.logInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -60,21 +61,32 @@
             this.accountToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.logInToolStripMenuItem});
             this.accountToolStripMenuItem.Name = "accountToolStripMenuItem";
-            this.accountToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
-            this.accountToolStripMenuItem.Text = "Account";
+            this.accountToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.accountToolStripMenuItem.Text = "0000";
             // 
             // logInToolStripMenuItem
             // 
             this.logInToolStripMenuItem.Name = "logInToolStripMenuItem";
             this.logInToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.logInToolStripMenuItem.Text = "Log in";
+            this.logInToolStripMenuItem.Text = "0001";
             this.logInToolStripMenuItem.Click += new System.EventHandler(this.logInToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(777, 409);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -94,6 +106,7 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem accountToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem logInToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- FBot/FBotInterface/MainForm.cs
+++ FBot/FBotInterface/MainForm.cs
@@ -2,12 +2,29 @@
 using System.Windows.Forms;
 using FBot;
 using FBotInterface.secondaryForms;
+using System.Resources;
+using System.Globalization;
+using System.Threading;
+using Microsoft.Win32;
+using System.Reflection;
 
 namespace FBotInterface
 {
     public partial class MainForm : Form
     {
         Fbot fb;
+
+        string strCulture;
+
+        private static ResourceManager rm;
+        public static ResourceManager RM
+        {
+            get
+            {
+                return rm;
+            }
+        }
+
 
         #region forms declarations
 
@@ -22,7 +39,12 @@ namespace FBotInterface
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            //this.accountToolStripMenuItem.Text = MainForm.RM.GetString("0000");
 
+            strCulture = "en-EN";
+
+            SetCulture();
+            SetResource();
         }
 
         private void LogIn()
@@ -34,6 +56,26 @@ namespace FBotInterface
         private void logInToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
+        }
+
+        #region language
+
+        private void SetCulture()
+        {
+            CultureInfo objCI = new CultureInfo(strCulture);
+            Thread.CurrentThread.CurrentCulture = objCI;
+            Thread.CurrentThread.CurrentUICulture = objCI;
+        }
+
+        private void SetResource()
+        {
+            rm = ResourceManager.CreateFileBasedResourceManager("resource", Application.StartupPath + @"\languagepacks", null);
+        }
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(rm.GetString("0002"));
         }
     }
 }
